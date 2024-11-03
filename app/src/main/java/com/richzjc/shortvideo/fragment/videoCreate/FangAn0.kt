69e0992a42da8e27761530009d86824cbca4fd38
richzjc <richzjc130109@gmail.com>
2024-11-03 19:17:41 +0800

    add files

--- app/src/main/java/com/richzjc/shortvideo/fragment/videoCreate/FangAn0.kt
+++ app/src/main/java/com/richzjc/shortvideo/fragment/videoCreate/FangAn0.kt
@@ -27,25 +27,38 @@ import java.io.FileOutputStream
 import java.io.InputStream
 import kotlin.math.max
 
-fun responseHeChengNBA(context: Context, originPath: List<String>?, statusTV: TextView?) {
-    requestData {
+private fun copyRawToFile(context: Context, rawResId: Int, outputFile: File) {
+    val inputStream: InputStream = context.resources.openRawResource(rawResId)
+    val outputStream = FileOutputStream(outputFile)
 
-        val imageFile = File(QDUtil.getShareImageCache(context).absolutePath, "imagePianTou")
-        if(!imageFile.exists()){
-            imageFile.mkdirs()
+    inputStream.use { input ->
+        outputStream.use { output ->
+            input.copyTo(output)
+        }
+    }
+}
+
+fun responseHePianTou(context: Context) {
+    requestData {
+        val file0 = File(QDUtil.getShareImageCache(context), "piantou.mp4")
+        if (!file0.exists()) {
+           copyRawToFile(context, R.raw.piantou, file0)
         }
 
-        var bitmap = BitmapFactory.decodeResource(context.resources, R.mipmap.fengmian)
-        var outputBitmap = Bitmap.createScaledBitmap(bitmap, 1080, 1920, false)
-        // 获取应用的内部存储路径
-        val fengMianFile = File(imageFile, "fengmian.png")
-        // 创建文件输出流
-        val fos = FileOutputStream(fengMianFile)
-        // 将 Bitmap 压缩为 JPEG 格式并写入文件流
-        outputBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
-        // 关闭文件流
-        fos.close()
+        val file1 = File(QDUtil.getShareImageCache(context), "piantou")
+        if(file1.exists())
+            return@requestData
 
+        file1.mkdirs()
+        delay(1000L)
+
+        val command = "-i ${file0.absolutePath} ${file1.absolutePath}/%d.png"
+        FFmpeg.execute(command)
+    }
+}
+
+fun responseHeChengNBA(context: Context, originPath: List<String>?, statusTV: TextView?) {
+    requestData {
         originPath?.forEachIndexed { index, s ->
             val file0 = File(QDUtil.getShareImageCache(context).absolutePath, "image${index}")
             if (file0.exists()) {
@@ -74,6 +87,54 @@ fun responseHeChengNBA(context: Context, originPath: List<String>?, statusTV: Te
                 val output = Config.getLastCommandOutput()
                 Log.e("FFmpeg Error", output)
             }
+        }
+    }
+}
+
+fun handlePianTou(context : Context){
+    // 加载图片
+    val file1 = File(QDUtil.getShareImageCache(context).absolutePath, "piantou_handle")
+    if (file1 != null) {
+        val listFiles = file1.listFiles()
+        listFiles?.forEach {
+            if (it.exists())
+                it.delete()
+        }
+    }
+
+    if (!file1.exists())
+        file1.mkdirs()
+
+
+    val file2 = File(QDUtil.getShareImageCache(context), "piantou")
+    val listFiles = file2.listFiles()
+    val paint = Paint()
+    // 设置画笔去掉透明度
+    paint.isAntiAlias = true
+    paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)
+    paint.alpha = 255
+    listFiles?.forEachIndexed { index, it ->
+        if(index < 25) {
+            var outputBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(outputBitmap)
+            canvas.drawColor(Color.BLACK)
+            var inputBitmap = BitmapFactory.decodeFile(it.absolutePath)
+            var inputHeight = (inputBitmap.height * 1f/inputBitmap.width) * 1080f
+            inputBitmap = Bitmap.createScaledBitmap(inputBitmap, 1080, inputHeight.toInt(), false)
+            canvas.drawBitmap(
+                inputBitmap,
+                (1080f - inputBitmap.width) / 2f,
+                (1920f - inputBitmap.height) / 2f,
+                paint
+            )
+            // 获取应用的内部存储路径
+            val imageFile = File(file1, it.name)
+            // 创建文件输出流
+            val fos = FileOutputStream(imageFile)
+            // 将 Bitmap 压缩为 JPEG 格式并写入文件流
+            outputBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+            // 关闭文件流
+            fos.close()
         }
     }
 }
@@ -113,7 +174,6 @@ fun processImage(
         paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OVER)
         paint.alpha = 255
 
-        var fengmian = BitmapFactory.decodeFile(File(File(QDUtil.getShareImageCache(context).absolutePath, "imagePianTou"), "fengmian.png").absolutePath)
 
         var commentBg = BitmapFactory.decodeFile(selectPicPath)
         var commentBgWidth = 1080 - ScreenUtils.dip2px(40f)
@@ -126,15 +186,22 @@ fun processImage(
 
         var followHintBitmap = BitmapFactory.decodeResource(context.resources, R.mipmap.follow_hint)
         var followHintBitmapWidth = commentBgWidth
-        var followHintBitmapHeight = (followHintBitmap.height * commentBgWidth) / (followHintBitmap.width * 1.0f)
+        var followHintBitmapHeight =
+            (followHintBitmap.height * commentBgWidth) / (followHintBitmap.width * 1.0f)
         followHintBitmap =
-            Bitmap.createScaledBitmap(followHintBitmap, followHintBitmapWidth, followHintBitmapHeight.toInt(), false)
-        followHintBitmap = getRoundedCornerBitmap(followHintBitmap, ScreenUtils.dip2px(20f).toFloat())
+            Bitmap.createScaledBitmap(
+                followHintBitmap,
+                followHintBitmapWidth,
+                followHintBitmapHeight.toInt(),
+                false
+            )
+        followHintBitmap =
+            getRoundedCornerBitmap(followHintBitmap, ScreenUtils.dip2px(20f).toFloat())
 
         var startX = 0
         var gap = (1920 - 1080) / 150f
         var isIncrease = true
-        var startIndex = listFiles.size/2
+        var startIndex = listFiles.size / 2
         listFiles?.forEachIndexed { index, it ->
             try {
                 updateStatusText(
@@ -165,10 +232,9 @@ fun processImage(
 
 
                 paint.alpha = 40
-                var startX = 1080 - (index%100) * 10.8f
-                canvas.drawBitmap(followHintBitmap, startX , 1920f/2, paint)
+                var startX = 1080 - (index % 100) * 10.8f
+                canvas.drawBitmap(followHintBitmap, startX, 1920f / 2, paint)
                 paint.alpha = 255
-
 
 
                 //绘制图片
@@ -183,27 +249,27 @@ fun processImage(
                         inputHeight.toInt(),
                         false
                     )
-                    val startX = (1080 - inputWidth)/2f
-                    val startY = (1920 - inputHeight)/2
+                    val startX = (1080 - inputWidth) / 2f
+                    val startY = (1920 - inputHeight) / 2
                     canvas.drawBitmap(
                         inputBitmap,
                         startX,
                         startY,
                         paint
                     )
-                } else if(index < startIndex + 30) {
+                } else if (index < startIndex + 30) {
                     val inputWidth = 1080 * 0.98f
                     val inputHeight = 1920 * 0.98f
-                    var value = (index - startIndex)%30
+                    var value = (index - startIndex) % 30
                     var realInputHeight = inputHeight - value * commentBgGap
-                    var realInputWidth = (inputWidth * realInputHeight)/inputHeight
+                    var realInputWidth = (inputWidth * realInputHeight) / inputHeight
                     inputBitmap = Bitmap.createScaledBitmap(
                         inputBitmap,
                         realInputWidth.toInt(),
                         realInputHeight.toInt(),
                         false
                     )
-                    val startX = (1080 - realInputWidth)/2f
+                    val startX = (1080 - realInputWidth) / 2f
                     val startY = 1920 * 0.01f
                     canvas.drawBitmap(
                         inputBitmap,
@@ -211,19 +277,19 @@ fun processImage(
                         startY,
                         paint
                     )
-                }else{
+                } else {
                     val inputWidth = 1080 * 0.98f
                     val inputHeight = 1920 * 0.98f
                     var value = 30
                     var realInputHeight = inputHeight - value * commentBgGap
-                    var realInputWidth = (inputWidth * realInputHeight)/inputHeight
+                    var realInputWidth = (inputWidth * realInputHeight) / inputHeight
                     inputBitmap = Bitmap.createScaledBitmap(
                         inputBitmap,
                         realInputWidth.toInt(),
                         realInputHeight.toInt(),
                         false
                     )
-                    val startX = (1080 - realInputWidth)/2f
+                    val startX = (1080 - realInputWidth) / 2f
                     val startY = 1920 * 0.01f
                     canvas.drawBitmap(
                         inputBitmap,
@@ -235,14 +301,14 @@ fun processImage(
 
 
                 if (index > startIndex && index < startIndex + 30) {
-                    var value = (index - startIndex)%30
+                    var value = (index - startIndex) % 30
                     canvas.drawBitmap(
                         commentBg,
                         (1080 - commentBg.width) / 2f,
                         (1920 - value * commentBgGap).toFloat(),
                         paint
                     )
-                }else if(index >= startIndex + 30){
+                } else if (index >= startIndex + 30) {
                     canvas.drawBitmap(
                         commentBg,
                         (1080 - commentBg.width) / 2f,
@@ -250,14 +316,6 @@ fun processImage(
                         paint
                     )
                 }
-
-
-                if(index < 20){
-                    paint.alpha = (255 - 25.5 * (index + 1) * 0.5).toInt()
-                    canvas.drawBitmap(fengmian, 0f, 0f, paint)
-                    paint.alpha = 255
-                }
-
 
 
                 if (index == 0) {
--- app/src/main/java/com/richzjc/shortvideo/fragment/videoCreate/ProcessFourBitmap.kt
+++ app/src/main/java/com/richzjc/shortvideo/fragment/videoCreate/ProcessFourBitmap.kt
@@ -20,10 +20,6 @@ import java.util.Arrays
 fun gennerateVideoNoAudio(originPath: String, context: Context, statusTV: TextView?, index: Int) {
     updateStatusText("开始生成第${index}个视频", statusTV)
     try {
-        val file = File(QDUtil.getShareImageCache(context).absolutePath, "video")
-        if (!file.exists())
-            file.mkdirs()
-
         // 创建一个 MediaMetadataRetriever 对象
         val retriever = MediaMetadataRetriever()
 
@@ -43,12 +39,16 @@ fun gennerateVideoNoAudio(originPath: String, context: Context, statusTV: TextVi
         val second = time.toLong() / 1000L
         val fps = frameCount * 1.0 / second
 
+        var f = File(QDUtil.getShareImageCache(context).absolutePath, "video")
+        if (f.exists())
+            f.delete()
+
+        f.mkdirs()
+
         val outputFile = File(
-            File(QDUtil.getShareImageCache(context).absolutePath, "video"),
+            f,
             "hecheng_noaudio${index}.mp4"
         )
-        if (outputFile.exists())
-            outputFile.delete()
 
 
         val file1 = File(QDUtil.getShareImageCache(context).absolutePath, "image_handle${index}")
@@ -65,13 +65,22 @@ fun gennerateVideoNoAudio(originPath: String, context: Context, statusTV: TextVi
             Integer.compare(num1, num2)
         }
 
-        val file2 = File(QDUtil.getShareImageCache(context).absolutePath, "imagePianTou")
+
+        val file2 = File(QDUtil.getShareImageCache(context).absolutePath, "piantou_handle")
         if (!file2.exists())
             file2.mkdirs()
 
         val listFiles2 = file2.listFiles()
         if (listFiles2 == null || listFiles2.size <= 0)
             return
+
+        Arrays.sort<File>(listFiles2) { f1: File, f2: File ->
+            val num1: Int = extractNumber(f1.getName())
+            val num2: Int = extractNumber(f2.getName())
+            Integer.compare(num1, num2)
+        }
+
+        val itemOffset = (listFiles2.size * 1f)/fps
 
         // 创建一个临时文本文件来存储图片路径
         val imageListPathFile = File(context?.getExternalFilesDir(null), "my_directory");
@@ -83,7 +92,6 @@ fun gennerateVideoNoAudio(originPath: String, context: Context, statusTV: TextVi
 
         // 设置帧率，假设每秒24帧
         val frameRate = fps
-        val offsetTime = listFiles2.size/fps
         // 构建FFmpeg命令
         val cmd =
             "-y -f concat -safe 0 -i $imageListPath -vsync vfr -pix_fmt yuv420p -r $frameRate -b:v 10M -s 1080x1920 -preset slow -crf 18 ${outputFile.absolutePath}"
@@ -92,7 +100,7 @@ fun gennerateVideoNoAudio(originPath: String, context: Context, statusTV: TextVi
         val returnCode = FFmpeg.execute(cmd)
         if (returnCode == Config.RETURN_CODE_SUCCESS) {
             updateStatusText("生成${index}个视频成功", statusTV)
-            heChengVideo(statusTV, context, originPath, index, offsetTime)
+            heChengVideo(statusTV, context, originPath, index, itemOffset)
         } else if (returnCode == Config.RETURN_CODE_CANCEL) {
             updateStatusText("生成${index}个视频取消", statusTV)
         } else {
@@ -104,52 +112,67 @@ fun gennerateVideoNoAudio(originPath: String, context: Context, statusTV: TextVi
     }
 }
 
-fun heChengVideo(statusTV: TextView?, context: Context, originPath: String?, index: Int, offsetTime : Double) {
+fun heChengVideo(statusTV: TextView?, context: Context, originPath: String?, index: Int, timeOffset : Double) {
     updateStatusText("合成第${index}个最终视频", statusTV)
 
     val file = File(QDUtil.getShareImageCache(context), "video")
     if (!file.exists())
         file.mkdirs()
 
-    val inputVideoPath = originPath ?: ""
+    var audioFile = File(file, "audio.mp4")
+    val command1 = "-i ${originPath} -q:a 0 -map a ${audioFile.absolutePath}"
+    var returnCode1 = FFmpeg.execute(command1)
+    if (returnCode1 == 0) {
+        updateStatusText("提取音频成功", statusTV)
+
+
     val inputVideoPath1 = File(file, "hecheng_noaudio${index}.mp4").absolutePath
 
 
-    var fileName = ""
-    if (index == 0)
-        fileName = "2"
-    else if (index == 1)
-        fileName = "4";
-    else if (index == 2)
-        fileName = "6"
-    else
-        fileName = "8"
+        var fileName = ""
+        if (index == 0)
+            fileName = "2"
+        else if (index == 1)
+            fileName = "4";
+        else if (index == 2)
+            fileName = "6"
+        else
+            fileName = "8"
 
-    
-    // 输出音频文件路径
-    val outputVideoPath = File(QDUtil.getShareImageCache(context), "${fileName}.mp4").absolutePath
-    val command = arrayOf<String>(
-        "-i", inputVideoPath,  // 第一个视频，音频来源
-        "-i", inputVideoPath1,  // 第二个视频，视频来源
-        "-c:v", "copy",  // 复制第二个视频的视频流
-        "-c:a", "aac",  // 使用 AAC 编码器来处理音频
-        "-map", "0:a:0",  // 从第一个视频中选择第一条音频流
-        "-map", "1:v:0",  // 从第二个视频中选择第一条视频流
-        "-shortest",  // 以较短的视频长度为基准
-        outputVideoPath // 输出合成后的视频文件路径
-    )
 
-    var returnCode = FFmpeg.execute(command)
-    if (returnCode == 0) {
-        updateStatusText("合成第${index}个视频成功", statusTV)
+        // 输出音频文件路径
+        val outputVideoPath = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "${fileName}.mp4").absolutePath
+
+        val command = "-itsoffset ${timeOffset} -i ${audioFile.absolutePath} -i ${inputVideoPath1} -c:v copy -c:a aac ${outputVideoPath}"
+
+//        val command = arrayOf<String>(
+//            "-i", inputVideoPath,  // 第一个视频，音频来源
+//            "-i", inputVideoPath1,  // 第二个视频，视频来源
+//            "-c:v", "copy",  // 复制第二个视频的视频流
+//            "-c:a", "aac",  // 使用 AAC 编码器来处理音频
+//            "-map", "0:a:0",  // 从第一个视频中选择第一条音频流
+//            "-map", "1:v:0",  // 从第二个视频中选择第一条视频流
+//            "-shortest",  // 以较短的视频长度为基准
+//            outputVideoPath // 输出合成后的视频文件路径
+//        )
+
+        var returnCode = FFmpeg.execute(command)
+        if (returnCode == 0) {
+            updateStatusText("合成第${index}个视频成功", statusTV)
+        } else {
+            Log.e("ffmpeg", Config.getLastCommandOutput())
+            updateStatusText("合成第${index}个视频失败", statusTV)
+        }
+
+
     } else {
         Log.e("ffmpeg", Config.getLastCommandOutput())
-        updateStatusText("合成第${index}个视频失败", statusTV)
+        updateStatusText("提取音频失败", statusTV)
     }
 }
 
 fun createImageListFile(
-    imagePathsPianTou : Array<File>,
+    imagePaths2: Array<File>,
     imagePaths: Array<File>,
     imageListPath: String,
     frameCount: Double
@@ -157,7 +180,7 @@ fun createImageListFile(
     try {
         val time = 1 / frameCount
         BufferedWriter(FileWriter(imageListPath)).use { writer ->
-            for (imagePath in imagePathsPianTou) {
+            for (imagePath in imagePaths2) {
                 writer.write("file '${imagePath.absolutePath}'\n")
                 // 设置每张图片显示的持续时间
                 writer.write("duration ${time}\n") // 每张图片显示时间（秒），24帧每秒 -> 1/24 ≈ 0.04
