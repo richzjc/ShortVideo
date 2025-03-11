794b6cd6ba11f2277e9eb14c180908f18a6ac559
richzjc <richzjc130109@gmail.com>
2025-03-11 17:02:04 +0800

    add files

--- app/src/main/java/com/richzjc/shortvideo/MainActFragmentModel.java
+++ app/src/main/java/com/richzjc/shortvideo/MainActFragmentModel.java
@@ -31,8 +31,8 @@ public class MainActFragmentModel {
         if (tabItemEntities == null)
             tabItemEntities = new ArrayList<>();
 
-//        tabItemEntities.add(new MainTabItemEntity(R.drawable.tab_drawable_news, "图片编辑", EditPicFragment.class, "editPic"));
-//        tabItemEntities.add(new MainTabItemEntity(R.drawable.tab_drawable_news, "视频制作", VideoCreateFragment.class, "videoCreate"));
+        tabItemEntities.add(new MainTabItemEntity(R.drawable.tab_drawable_news, "图片编辑", EditPicFragment.class, "editPic"));
+        tabItemEntities.add(new MainTabItemEntity(R.drawable.tab_drawable_news, "视频制作", VideoCreateFragment.class, "videoCreate"));
         tabItemEntities.add(new MainTabItemEntity(R.drawable.tab_drawable_news, "图片视频", PicVideoFragment.class, "videoCreate"));
     }
 }
