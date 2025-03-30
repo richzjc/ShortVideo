af99bdb02f1090b8aa4e012fa912b8f29413e3e0
richzjc <richzjc130109@gmail.com>
2025-03-30 18:26:04 +0800

    添加了方案6

--- app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan1.kt
+++ app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan1.kt
@@ -36,7 +36,7 @@ suspend fun fangan1(
     paint: Paint
 ) {
     delay(30)
-    (0 until 90)?.forEach {
+    (0 until 60)?.forEach {
         if (handleFile.listFiles().size < totalCount) {
             if (it < 30) {
                 fangan1Small30(preBitmap, curBitmap, paint, handleFile, status, it)
@@ -57,8 +57,8 @@ private suspend fun fang1Large30(
     delay(30)
     paint.alpha = 255
 
-    val realWidth = pBitmap.width * (1 + 0.01f * (index - 60 + 1))
-    val realHeight = pBitmap.height * (1 + 0.01f * (index - 60 + 1))
+    val realWidth = 1080 + 108 - calculateCos(index + 1 , 30, 108f).toInt()
+    val realHeight = 1920 + 192 - calculateCos(index + 1, 30, 192f).toInt()
     val preBitmap = Bitmap.createScaledBitmap(pBitmap, realWidth.toInt(), realHeight.toInt(), true)
     var outputBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(outputBitmap)
@@ -80,17 +80,19 @@ private suspend fun fangan1Small30(
     var outputBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(outputBitmap)
 
-    var bgBitmap = BitmapFactory.decodeResource(
-        UtilsContextManager.getInstance().application.resources,
-        R.mipmap.imgnew1
-    )
-    bgBitmap = Bitmap.createScaledBitmap(bgBitmap, 1080, 1920, true)
-    canvas.drawBitmap(bgBitmap!!, 0f, 0f, paint)
 
-    val preAlpha = 255 - calculateCos(index + 1, 5, 255f)
-    if (preAlpha > 0) {
-        paint.alpha = preAlpha.toInt()
-        canvas.drawBitmap(preBitmap, 0f, 0f, paint)
+    var blurBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
+    val blurCanvas = Canvas(blurBitmap)
+    blurCanvas.drawBitmap(curBitmap, 0f, 0f, paint)
+    blurBitmap = blur(blurBitmap)
+    canvas.drawBitmap(blurBitmap!!, 0f, 0f, paint)
+
+    if (index < 5) {
+        val preAlpha = 255 - calculateCos(index + 1, 5, 255f)
+        if (preAlpha > 0) {
+            paint.alpha = preAlpha.toInt()
+            canvas.drawBitmap(preBitmap, 0f, 0f, paint)
+        }
     }
 
     paint.alpha = 255
--- app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan2.kt
+++ app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan2.kt
@@ -6,6 +6,9 @@ import android.graphics.Color
 import android.graphics.Matrix
 import android.graphics.Paint
 import android.widget.TextView
+import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculateCos
+import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculateSin
+import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculatex2
 import kotlinx.coroutines.delay
 import java.io.File
 
@@ -76,17 +79,18 @@ private suspend fun fangan1Small30(
     blurBitmap = blur(blurBitmap)
     canvas.drawBitmap(blurBitmap, 0f, 0f, paint)
 
-    val preAlpha = (255 - (255 / 15f) * index)
-    if (preAlpha > 0) {
-        paint.alpha = preAlpha.toInt()
-        canvas.drawBitmap(preBitmap, 0f, 0f, paint)
+    if (index < 5) {
+        val preAlpha = 255 - calculateCos(index + 1, 5, 255f)
+        if (preAlpha > 0) {
+            paint.alpha = preAlpha.toInt()
+            canvas.drawBitmap(preBitmap, 0f, 0f, paint)
+        }
     }
 
     paint.alpha = 255
-    val progress = (index + 1) / 30f
     val matrix = Matrix()
-    val degrees = 360f * progress
-    val scale = 0.2f + progress * 0.9f
+    val degrees = calculateSin(index + 1, 30, 360f)
+    val scale = 0.2f + calculateSin(index + 1, 30, 0.9f)
     matrix.apply {
         reset()
         postScale(scale, scale, 1080 / 2f, 1920 / 2f)
@@ -94,7 +98,15 @@ private suspend fun fangan1Small30(
     }
 
     // 绘制动态图
-    canvas.drawBitmap(curBitmap, matrix, null)
+    var radius = 99 - calculatex2(index + 1, 30, 99f).toInt()
+    if (radius % 2 == 0)
+        radius -= 1
+
+    if (radius <= 0)
+        radius = 1
+
+    val rbitmap = blur(curBitmap, radius)
+    canvas.drawBitmap(rbitmap, matrix, null)
     canvas.drawColor(Color.parseColor("#1132cd32"))
     saveBitmapToFile(outputBitmap, handleFile, status)
 }
