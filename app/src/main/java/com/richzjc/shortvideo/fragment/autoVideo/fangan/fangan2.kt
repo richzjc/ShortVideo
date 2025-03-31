e3e75ff2eb51aa048e81ed7d778e0e65dad6afad
richzjc <richzjc130109@gmail.com>
2025-03-31 23:36:42 +0800

    添加了方案6

--- app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan2.kt
+++ app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan2.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculateCos
 import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculateSin
 import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculatex2
+import com.richzjc.shortvideo.fragment.videoCreate.getRoundedCornerBitmap
+import com.richzjc.shortvideo.util.ScreenUtils
 import kotlinx.coroutines.delay
 import java.io.File
 
@@ -25,9 +27,9 @@ suspend fun fangan2(
 ) {
     delay(30)
     val blurBg :Bitmap = blur(curBitmap)
-    (0 until 60)?.forEach {
+    (0 until 120)?.forEach {
         if (handleFile.listFiles().size < totalCount) {
-            if (it < 30) {
+            if (it < 60) {
                 fangan1Small30(blurBg,  preBitmap, curBitmap, paint, handleFile, status, it)
             } else {
                 fang1Large30(curBitmap, paint, handleFile, status, it)
@@ -45,17 +47,17 @@ private suspend fun fang1Large30(
 ) {
     delay(30)
     paint.alpha = 255
-    val widthGap = (pBitmap.width * 0.1f) / 30f
-    val heightGap = (pBitmap.height * 0.1f) / 30f
+    val widthGap = (pBitmap.width * 0.1f) / 60f
+    val heightGap = (pBitmap.height * 0.1f) / 60f
 
-    val realWidth = pBitmap.width * 1.1f - widthGap * (index - 30 + 1)
-    val realHeight = pBitmap.height * 1.1f - heightGap * (index - 30 + 1)
+    val realWidth = pBitmap.width * 1.1f - widthGap * (index - 60 + 1)
+    val realHeight = pBitmap.height * 1.1f - heightGap * (index - 60 + 1)
     val preBitmap = Bitmap.createScaledBitmap(pBitmap, realWidth.toInt(), realHeight.toInt(), true)
 
 
     var outputBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(outputBitmap)
-    canvas.drawBitmap(preBitmap, (1080 - realWidth) / 2f, (1920 - realHeight) / 2f, paint)
+    canvas.drawBitmap(preBitmap, (1080 - realWidth) / 2f, (1920 - realHeight)/2, paint)
     canvas.drawColor(Color.parseColor("#1132cd32"))
     saveBitmapToFile(outputBitmap, file1, status)
 }
@@ -88,8 +90,8 @@ private suspend fun fangan1Small30(
 
     paint.alpha = 255
     val matrix = Matrix()
-    val degrees = calculateSin(index + 1, 30, 360f)
-    val scale = 0.2f + calculateSin(index + 1, 30, 0.9f)
+    val degrees = calculateSin(index + 1, 60, 360f)
+    val scale = 0.2f + calculateSin(index + 1, 60, 0.9f)
     matrix.apply {
         reset()
         postScale(scale, scale, 1080 / 2f, 1920 / 2f)
@@ -97,7 +99,7 @@ private suspend fun fangan1Small30(
     }
 
     // 绘制动态图
-    var radius = 99 - calculatex2(index + 1, 30, 99f).toInt()
+    var radius = 55 - calculatex2(index + 1, 60, 55f).toInt()
     if (radius % 2 == 0)
         radius -= 1
 
@@ -105,7 +107,10 @@ private suspend fun fangan1Small30(
         radius = 1
 
     val rbitmap = blur(curBitmap, radius)
-    canvas.drawBitmap(rbitmap, matrix, null)
+    val maxRadius = ScreenUtils.dip2px(20f)
+    val roundRadius = maxRadius - calculatex2(index + 1, 60, maxRadius.toFloat()).toInt()
+    val roundBitmap = getRoundedCornerBitmap(rbitmap, roundRadius.toFloat())
+    canvas.drawBitmap(roundBitmap, matrix, null)
     canvas.drawColor(Color.parseColor("#1132cd32"))
     saveBitmapToFile(outputBitmap, handleFile, status)
 }
--- app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan3.kt
+++ app/src/main/java/com/richzjc/shortvideo/fragment/autoVideo/fangan/fangan3.kt
@@ -6,8 +6,11 @@ import android.graphics.Color
 import android.graphics.Matrix
 import android.graphics.Paint
 import android.widget.TextView
+import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculateCos
 import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculateSin
 import com.richzjc.shortvideo.fragment.autoVideo.fangan.interpreter.calculatex2
+import com.richzjc.shortvideo.fragment.videoCreate.getRoundedCornerBitmap
+import com.richzjc.shortvideo.util.ScreenUtils
 import kotlinx.coroutines.delay
 import java.io.File
 
@@ -23,7 +26,7 @@ suspend fun fangan3(
     paint: Paint
 ) {
     delay(30)
-    val blurBg :Bitmap = blur(curBitmap)
+    val blurBg: Bitmap = blur(curBitmap)
     val preBitmapList = ArrayList<Bitmap>()
     val height = preBitmap.height / 4
     val bitmap1 = Bitmap.createBitmap(preBitmap, 0, 0, preBitmap.width, height, null, false)
@@ -38,10 +41,10 @@ suspend fun fangan3(
     preBitmapList.add(bitmap3)
     preBitmapList.add(bitmap4)
 
-    (0 until 60)?.forEach {
+    (0 until 120)?.forEach {
         if (handleFile.listFiles().size < totalCount) {
-            if (it < 30) {
-                fangan1Small30(blurBg,preBitmapList, curBitmap, paint, handleFile, status, it)
+            if (it < 60) {
+                fangan1Small30(blurBg, preBitmapList, curBitmap, paint, handleFile, status, it)
             } else {
                 fang1Large30(curBitmap, paint, handleFile, status, it)
             }
@@ -58,23 +61,20 @@ private suspend fun fang1Large30(
 ) {
     delay(30)
     paint.alpha = 255
-    val widthGap = (pBitmap.width * 0.05f) / 30f
-    val heightGap = (pBitmap.height * 0.05f) / 30f
 
-    val realWidth = pBitmap.width * 1.05f - widthGap * (index - 30 + 1)
-    val realHeight = pBitmap.height * 1.05f - heightGap * (index - 30 + 1)
+    val realWidth = 1080 + 108 - calculateCos(index + 1, 60, 108f).toInt()
+    val realHeight = 1920 + 192 - calculateCos(index + 1, 60, 192f).toInt()
     val preBitmap = Bitmap.createScaledBitmap(pBitmap, realWidth.toInt(), realHeight.toInt(), true)
-
     var outputBitmap = Bitmap.createBitmap(1080, 1920, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(outputBitmap)
-    canvas.drawBitmap(preBitmap, (1080 - realWidth) / 2f, (1920 - realHeight) / 2f, paint)
+    canvas.drawBitmap(preBitmap, (1080 - realWidth) / 2f, -(1920 - realHeight).toFloat(), paint)
     canvas.drawColor(Color.parseColor("#1132cd32"))
     saveBitmapToFile(outputBitmap, file1, status)
 }
 
 
 private suspend fun fangan1Small30(
-    blurBg : Bitmap,
+    blurBg: Bitmap,
     preBitmapList: List<Bitmap>,
     curBitmap: Bitmap,
     paint: Paint,
@@ -90,25 +90,19 @@ private suspend fun fangan1Small30(
     canvas.drawBitmap(blurBitmap!!, 0f, 0f, paint)
 
 
-    if (index < 15) {
-        var alpha = (index + 1) * (255 / 15f)
-        if (alpha > 255)
-            alpha = 255f
-        paint.alpha = alpha.toInt()
-    } else {
-        paint.alpha = 255
+    paint.alpha = 255
+    val realWidth = calculateSin(index + 1, 60, 1080f).toInt()
+    val realHeight = calculateSin(index + 1, 60, 1920f).toInt()
+    if (realWidth > 0 && realHeight > 0) {
+        val preBitmap =
+            Bitmap.createScaledBitmap(curBitmap, realWidth.toInt(), realHeight.toInt(), true)
+        val roundRadius =
+            realWidth / 2 - calculatex2(index + 1, 60, realWidth.toFloat() / 2).toInt()
+        val roundBitmap = getRoundedCornerBitmap(preBitmap, roundRadius.toFloat())
+        canvas.drawBitmap(roundBitmap, (1080 - realWidth) / 2f, (1920 - realHeight) / 2f, paint)
     }
 
-    val widthGap1 = (1080 * 0.05f) / 30f
-    val heightGap1 = (1920 * 0.05f) / 30f
 
-    val realWidth = 1080 + widthGap1 * (index + 1)
-    val realHeight = 1920 + heightGap1 * (index + 1)
-    val preBitmap =
-        Bitmap.createScaledBitmap(curBitmap, realWidth.toInt(), realHeight.toInt(), true)
-    canvas.drawBitmap(preBitmap, (1080 - realWidth) / 2f, (1920 - realHeight) / 2f, paint)
-
-    val widthGap = 1080 / 30f
     val heightGap = 1920 / 4f
     paint.alpha = 255
     var blurValue: Int
@@ -120,14 +114,18 @@ private suspend fun fangan1Small30(
     if (blurValue % 2 == 0)
         blurValue += 1
 
+    val maxRadius = ScreenUtils.dip2px(20f)
+    val roundRadius = maxRadius - calculatex2(index + 1, 60, maxRadius.toFloat()).toInt()
+
     preBitmapList.forEachIndexed { innerIndex, bitmap ->
         val bmp = blur(bitmap, blurValue)
+        val roundBitmap = getRoundedCornerBitmap(bmp, roundRadius.toFloat())
         if (innerIndex % 2 == 0) {
-            val startX = (index + 1) * widthGap
-            canvas.drawBitmap(bmp, startX, innerIndex * heightGap, paint)
+            val startX = calculateSin(index + 1, 60, 1080f)
+            canvas.drawBitmap(roundBitmap, startX, innerIndex * heightGap, paint)
         } else {
-            val startX = -(index + 1) * widthGap
-            canvas.drawBitmap(bmp, startX, innerIndex * heightGap, paint)
+            val startX = -calculateSin(index + 1, 60, 1080f)
+            canvas.drawBitmap(roundBitmap, startX, innerIndex * heightGap, paint)
         }
     }
     canvas.drawColor(Color.parseColor("#1132cd32"))
